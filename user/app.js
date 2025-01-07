6a0a646619e070db38962184c34841841411cef9
Dev Nehate <ndev7567@gmail.com>
2025-01-07 23:11:51 +0530

    mongodb coonected

--- user/app.js
+++ user/app.js
@@ -2,9 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/user.routes.js');
 const cookieParser = require('cookie-parser');
+const connect = require('./db/db.js');
 
+connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
