ee167acb8eeaeb1d574b6350ac2c81855538e0be
Dev Nehate <ndev7567@gmail.com>
2025-01-08 14:57:55 +0530

    user done

--- user/controller/user.controller.js
+++ user/controller/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklisttokenModel = require('../models/blacklisttoken.model.js');
 
 module.exports.register = async (req, res) => {
     try {
@@ -20,7 +21,52 @@ module.exports.register = async (req, res) => {
 
         res.cookie('token', token);
 
-        res.send({message: 'User created successfully'});
+        res.send({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+}
+
+module.exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.cookie('token', token);
+
+        res.send({ message: 'User logged in successfully' });
+
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+}
+
+module.exports.logout = async (req, res) => {
+    try {
+        const token = req.cookies.token;
+        await blacklisttokenModel.create({ token });
+        res.clearCookie('token');
+        res.send({ message: 'User logged out successfully' });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+}
+
+module.exports.profile = async (req, res) => {
+    try {
+        res.send(req.user);
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
--- user/routes/user.routes.js
+++ user/routes/user.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller.js');
+const authMiddleware = require('../middleware/authMiddleware.js');
 
 
 router.post('/register', userController.register);
+router.post('/login', userController.login)
+router.get('/logout', userController.logout);
+router.get('/profile',authMiddleware.userAuth,userController.profile);
 
 module.exports = router;
